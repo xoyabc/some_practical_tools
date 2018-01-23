80b86124561a7ee065f1c7b6da0486873d46a5b6
xoyabc <1031138448@qq.com>
2018-01-24 01:16:46 +0800

    add some description
    add some description

--- movie_relevant/get_available_tracker.sh
+++ movie_relevant/get_available_tracker.sh
@@ -4,8 +4,9 @@
 # * Email         : 1031138448@qq.com
 # * Last modified : 2018-01-24 00:55
 # * Filename      : get_available_tracker.sh
-# * Description   : get available tracker address
+# * Description   : get available tracker address and output to valid_tracerk.txt
 # * ********************************************************
+
 # define a output function
 warn_echo()
 {
@@ -17,6 +18,15 @@ info_echo()
     echo -e "\033[40;32m[Info]: $1 \033[0m"
 }
 
+Usage ()
+{
+    echo "Usage:sh $0 tracer_file"
+    exit 0
+}
+[ $# -ne 1 ] && Usage
+TRACKER_FILE="$1"
+AVAILABLE_TRACKER="valid_tracerk.txt"
+> ${AVAILABLE_TRACKER}
 # check if a port is reachable
 function check_port ()
 {
@@ -33,7 +43,7 @@ function check_port ()
  
 }
 
-cat test.file |while read line
+cat ${TRACKER_FILE} |while read line
 do
     tracker_info=$(echo "${line}" |sed -r 's#([^/]*)//([^/:]*)(:?)([0-9]{0,5})(/?).*$#\2 \4#g')
     host=$(echo "${tracker_info}" |awk '{print $1}')
@@ -42,7 +52,7 @@ do
     #echo "${host} ${port}"
     if `check_port ${host} ${port} |grep -q open`
     then
-        echo ${line}
+        echo ${line} >> ${AVAILABLE_TRACKER}
     else
         continue
     fi
