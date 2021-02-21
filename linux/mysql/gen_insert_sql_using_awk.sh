d951fdeb3351e3e4a9de107786b300726e09545b
xoyabc <lxh1031138448@gmail.com>
2021-02-21 22:47:28 +0800

    Update gen_insert_sql_using_awk.sh

--- linux/mysql/gen_insert_sql_using_awk.sh
+++ linux/mysql/gen_insert_sql_using_awk.sh
@@ -15,13 +15,17 @@ group_num=5000
 total_num=$(cat ${FILE} |wc -l)
 
 awk -v t_num="${total_num}" -v g_num="${group_num}" '
+# 只有一个数据或分组是 1
 { if((NR%g_num==1 && NR==t_num) || g_num==1)
       printf "INSERT INTO `PreTempConfId` (`tempConfId`, `useFlag`) VALUES (\047%s\047, \0470\047);\n", $1;
   else {
+      # 第一行，结尾为逗号
       if (NR%g_num==1)
           printf "INSERT INTO `PreTempConfId` (`tempConfId`, `useFlag`) VALUES (\047%s\047, \0470\047),\n", $1;
+      # 中间行，结尾为逗号，无 insert 只有要插入的数据
       else if ((NR%g_num!=0 && NR%g_num!=1) && NR!=t_num)
           printf "(\047%s\047, \0470\047),\n", $1;
+      # 末尾行，结尾为分号，无 insert 只有要插入的数据
       else if ((NR%g_num==0) || NR==t_num)
           printf "(\047%s\047, \0470\047);\n", $1;
      }
